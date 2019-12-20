387beb94d809b82e7fab19a6616a857d72a9db09
Hectorlobo10000 <hector.lobo10000@gmail.com>
2019-12-20 11:07:05 -0600

    Fixed

--- ReadFiles.py
+++ ReadFiles.py
@@ -17,18 +17,18 @@ ws.cell(column=4, row=1, value='Tamano en bytes')
 ws.cell(column=5, row=1, value='Observacion')
 row = 2
 for line in openedFile:
-  tokens = line.split(' --- ')
   if not 'Total seconds' in line:
+    tokens = line.split(' --- ')
     if not '\n' in tokens[0]:
       if 'ERROR DE ARCHIVO' in line:
         for col in range(1, 6):
-          ws.cell(column=col, row=row, value=tokens[col - 1])
+          ws.cell(column=col, row=row, value=tokens[col - 1].replace('\n', ''))
       elif 'FILE MOVED' in line:
         for col in range(1, 6):
-          ws.cell(column=col, row=row, value=tokens[col - 1])
+          ws.cell(column=col, row=row, value=tokens[col - 1].replace('\n', ''))
       else:
         for col in range(1, 5):
-          ws.cell(column=col, row=row, value=tokens[col - 1])
+          ws.cell(column=col, row=row, value=tokens[col - 1].replace('\n', ''))
       row = row + 1
       print(row - 1)
   else: 
